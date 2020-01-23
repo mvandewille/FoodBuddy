786d9be7b02d91992dcd2f1facfcd0367052ba23
Max Van de Wille <maxv1@iastate.edu>
2020-01-23 11:35:26 -0600

    Basic iOS mockup of app functions

---
+++ Experiments/MaxV/iOS UI Mockup/iOS UI Mockup/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  iOS UI Mockup
+//
+//  Created by Max Van de Wille on 1/23/20.
+//  Copyright © 2020 Max Van de Wille. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
