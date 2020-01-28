2ec77d52790259c27193682e66189126faa987d0
Sam Massey <samassey@iastate.edu>
2020-01-28 16:18:27 -0600

    Run part 2 of demo with different messages.

--- Experiments/Sam Massey/Experiment #3/part2demo/src/main/java/com/example/demo/GreetingController.java
+++ Experiments/Sam Massey/Experiment #3/part2demo/src/main/java/com/example/demo/GreetingController.java
@@ -14,6 +14,7 @@ public class GreetingController {
 
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+		//return new Greeting(counter.incrementAndGet(), String.format(template, name));
+		return new Greeting(counter.incrementAndGet(), "Test #2 :O");
 	}
 }
