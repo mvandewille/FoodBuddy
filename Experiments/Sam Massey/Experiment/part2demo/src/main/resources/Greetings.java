fa7e15957f5ae45572aaf25a96080b9879efb7dd
Sam Massey <samassey@iastate.edu>
2020-01-27 20:10:05 -0600

    creating app.

---
+++ Experiments/Sam Massey/Experiment/part2demo/src/main/resources/Greetings.java
@@ -0,0 +1,20 @@
+package com.example.restservice;
+
+public class Greetings {
+
+	private final long id;
+	private final String content;
+
+	public Greetings(long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+}
