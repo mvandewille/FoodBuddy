0ddeb3375dfb8c24149b65c58fb42704f38e98f2
Sam Massey <samassey@iastate.edu>
2020-01-27 19:53:31 -0600

    Start doing second part of demo. First part keeps failing from an error that has yet to be solved. Error is documentted in Piazza.

---
+++ Experiments/Sam Massey/Experiment/part2demo/src/main/java/com/example/demo/Part2demoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Part2demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Part2demoApplication.class, args);
+	}
+
+}
