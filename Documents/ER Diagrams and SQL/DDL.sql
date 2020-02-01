87f264e39f4678885c07e4a6ebac68221f58afd7
root <root@coms-309-hv-3.cs.iastate.edu>
2020-02-01 15:48:03 -0600

    SQL changes

--- Documents/ER Diagrams and SQL/DDL.sql
+++ Documents/ER Diagrams and SQL/DDL.sql
@@ -1,5 +1,5 @@
 set FOREIGN_KEY_CHECKS = 0;
-CREATE DATABASE IF NOT EXISTS 'app_db';
+CREATE DATABASE IF NOT EXISTS app_db;
 
 USE app_db;
 
@@ -31,7 +31,7 @@ DROP TABLE IF EXISTS prof_picture;
 CREATE TABLE prof_picture(
 	user_email varchar(100) UNIQUE,
 	picture image,
-	foreign key(user_email) references user.email
+	foreign key(user_email) references `user` (`email`)
 );
 
 
@@ -46,7 +46,7 @@ CREATE TABLE day(
 	protein int,
 	fat int,
 	cholesterol int
-	foreign key(user_email) references user.email
+	foreign key(user_email) references `user`(`email`)
 );
 
 
@@ -54,10 +54,10 @@ DROP TABLE IF EXISTS status;
 
 CREATE TABLE status(
 	poster_email varchar(100),
-	statusID int, 
+	statusID int,
 	referenced_day date,
 	message varchar(200)
-	foreign key(poster_email) references user.email,
+	foreign key(poster_email) references `user`(`email`),
 	primary key(statusID)
 );
 
@@ -67,8 +67,8 @@ DROP TABLE IF EXISTS friends;
 CREATE TABLE friends(
 	user1_email varchar(100),
 	user2_email varchar(100),
-	foreign key(user1_email) references user.email,
-	foreign key(user2_email) references user.email
+	foreign key(user1_email) references `user`(`email`),
+	foreign key(user2_email) references `user`(`email`)
 );
 
 
@@ -77,8 +77,8 @@ DROP TABLE IF EXISTS allergic;
 CREATE TABLE allergic(
 	user_email varchar(100),
 	food_name varchar(100),
-	foreign key(food) references allergen.food_name,
-	foreign key(user_email) references user.email
+	foreign key(food) references allergen(food_name),
+	foreign key(user_email) references user(email)
 );
 
 SET FOREIGN_KEY_CHECKS = 1;
