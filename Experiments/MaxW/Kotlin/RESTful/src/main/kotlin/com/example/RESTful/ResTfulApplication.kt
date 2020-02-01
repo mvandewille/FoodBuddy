5535ca54101aae62c562c123a5a9f6d448c6a634
BuildTools <unconfigured@null.spigotmc.org>
2020-01-31 19:23:20 -0600

    Kotlin RESTful API testing

---
+++ Experiments/MaxW/Kotlin/RESTful/src/main/kotlin/com/example/RESTful/ResTfulApplication.kt
@@ -0,0 +1,11 @@
+package com.example.RESTful
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ResTfulApplication
+
+fun main(args: Array<String>) {
+	runApplication<ResTfulApplication>(*args)
+}
