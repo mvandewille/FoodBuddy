a63068fe76bb1a87d442e6a3658aa746371b4a69
Max Van de Wille <maxv1@iastate.edu>
2020-02-01 15:52:18 -0600

    Changes to DDL for SQL Server syntax

--- Documents/ER Diagrams and SQL/DDL.sql
+++ Documents/ER Diagrams and SQL/DDL.sql
@@ -14,31 +14,29 @@ CREATE TABLE allergen(
 
 DROP TABLE IF EXISTS user;
 
-CREATE TABLE user(
-	email varchar(100),
+CREATE TABLE app_user(
+	email varchar(100) PRIMARY KEY,
 	name varchar(150),
 	user_password varchar(100) NOT NULL,
 	user_type varchar(20),
 	height_in int,
 	weight_lb int,
 	calorie_limit int,
-	primary key(email)
 );
 
 
 DROP TABLE IF EXISTS prof_picture;
 
 CREATE TABLE prof_picture(
-	user_email varchar(100) UNIQUE,
-	picture image,
-	foreign key(user_email) references `user` (`email`)
+	user_email varchar(100) FOREIGN KEY references app_user(email),
+	picture_path varchar(100)
 );
 
 
 DROP TABLE IF EXISTS day;
 
 CREATE TABLE day(
-	user_email varchar(100) UNIQUE,
+	user_email varchar(100) FOREIGN KEY references app_user(email),
 	stored_date date,
 	calories int,
 	carbohydrates int,
@@ -46,39 +44,32 @@ CREATE TABLE day(
 	protein int,
 	fat int,
 	cholesterol int
-	foreign key(user_email) references `user`(`email`)
 );
 
 
 DROP TABLE IF EXISTS status;
 
 CREATE TABLE status(
-	poster_email varchar(100),
-	statusID int,
+	poster_email varchar(100) FOREIGN KEY references app_user(email),
+	statusID int PRIMARY KEY,
 	referenced_day date,
 	message varchar(200)
-	foreign key(poster_email) references `user`(`email`),
-	primary key(statusID)
 );
 
 
 DROP TABLE IF EXISTS friends;
 
 CREATE TABLE friends(
-	user1_email varchar(100),
-	user2_email varchar(100),
-	foreign key(user1_email) references `user`(`email`),
-	foreign key(user2_email) references `user`(`email`)
+	user1_email varchar(100) FOREIGN KEY references app_user(email),
+	user2_email varchar(100) FOREIGN KEY references app_user(email),
 );
 
 
 DROP TABLE IF EXISTS allergic;
 
 CREATE TABLE allergic(
-	user_email varchar(100),
-	food_name varchar(100),
-	foreign key(food) references allergen(food_name),
-	foreign key(user_email) references user(email)
+	user_email varchar(100)	foreign key references user(email),
+	food_name varchar(100) foreign key references allergen(food_name),
 );
 
 SET FOREIGN_KEY_CHECKS = 1;
