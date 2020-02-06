84331f38c83fece6b3d1e8b5bf58bd0f0b1a5c27
Sam Massey <samassey@iastate.edu>
2020-02-06 15:03:14 -0600

    Start with the initializations and functions.

--- Experiments/Sam Massey/ProjectWork/fbBackend/src/main/kotlin/com/main/fbBackend/User.kt
+++ Experiments/Sam Massey/ProjectWork/fbBackend/src/main/kotlin/com/main/fbBackend/User.kt
@@ -1,2 +1,14 @@
 package com.main.fbBackend
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.dao.EmptyResultDataAccessException
+import java.util.concurrent.atomic.AtomicLong
+
+@RestController
+class UserController{
+    @GetMapping
+    fun 
+}
