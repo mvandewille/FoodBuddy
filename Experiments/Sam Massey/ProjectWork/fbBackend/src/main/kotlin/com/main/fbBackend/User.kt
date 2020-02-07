71f59fa508fc63cd7ac04af63534f516712b6d37
Sam Massey <samassey@iastate.edu>
2020-02-07 14:33:07 -0600

    Add to User Controller.

--- Experiments/Sam Massey/ProjectWork/fbBackend/src/main/kotlin/com/main/fbBackend/User.kt
+++ Experiments/Sam Massey/ProjectWork/fbBackend/src/main/kotlin/com/main/fbBackend/User.kt
@@ -5,10 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.dao.EmptyResultDataAccessException
+import java.net.FileNameMap
 import java.util.concurrent.atomic.AtomicLong
 
 @RestController
 class UserController{
-    @GetMapping("/")
-    fun getUser() = "User 1"
+    @GetMapping("/addUser")
+    fun addUser(@RequestParam(value = "fName", required = true) fileNameMap: fName = String, @RequestParam(value = "lName", requiredd = true) lName = String): String {
+        println("--------------------------------")
+
+    }
 }
