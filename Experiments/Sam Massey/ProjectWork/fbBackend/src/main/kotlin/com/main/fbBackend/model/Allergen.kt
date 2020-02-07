f09aea6c62d0184e90c37d12b23ff8fb67a72000
Sam Massey <samassey@iastate.edu>
2020-02-07 15:44:53 -0600

    Finish the Allergen.kt class

--- Experiments/Sam Massey/ProjectWork/fbBackend/src/main/kotlin/com/main/fbBackend/model/Allergen.kt
+++ Experiments/Sam Massey/ProjectWork/fbBackend/src/main/kotlin/com/main/fbBackend/model/Allergen.kt
@@ -1,5 +1,11 @@
 package com.main.fbBackend.model
 
-class Allergen {
+import org.springframework.data.annotation.Id
 
+class Allergen (@Id private val Name: String){
+
+    //Override the toString function to what we want it to be.
+    override fun toString(): String {
+        return "Allergen[Name=$Name]"
+    }
 }
--- Experiments/Sam Massey/ProjectWork/fbBackend/src/main/kotlin/com/main/fbBackend/repository/AllergenRepository.kt
+++ Experiments/Sam Massey/ProjectWork/fbBackend/src/main/kotlin/com/main/fbBackend/repository/AllergenRepository.kt
@@ -1,4 +1,11 @@
 package com.main.fbBackend.repository
 
-class AllergenRepository {
+import org.springframework.data.annotation.Id
+
+class AllergenRepository (@Id private val Name: String){
+
+    //Override the toString function to what we want it to be.
+    override fun toString(): String {
+        return "Allergen[Name=$Name]"
+    }
 }
