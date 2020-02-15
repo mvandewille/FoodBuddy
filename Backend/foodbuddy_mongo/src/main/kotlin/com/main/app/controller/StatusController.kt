ee09f4655ff1a08c67d39200925854286f88f2c9
Sam Massey <samassey@iastate.edu>
2020-02-15 16:30:44 -0600

    Added it to one working area.

---
+++ Backend/foodbuddy_mongo/src/main/kotlin/com/main/app/controller/StatusController.kt
@@ -0,0 +1,59 @@
+package com.main.fbBackend.Controller
+
+import com.main.fbBackend.model.Status
+import com.main.fbBackend.repository.StatusRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.dao.EmptyResultDataAccessException
+import org.springframework.web.bind.annotation.RequestMapping
+
+@RestController
+@RequestMapping("/status")
+class StatusController {
+    @Autowired
+    lateinit var repository: StatusRepository
+
+    @GetMapping("/add")
+    fun addStatus(@RequestParam(value = "name", required = true) name: String,
+                  @RequestParam(value = "message", required = true) message: String,
+                  @RequestParam(value = "flagged", required = true) flagged: Boolean): String {
+
+        val i = getLastAdd()
+        val temp = Status(name, message, flagged, i + 1L)
+        repository.save(temp)
+        return "Added new $temp"
+    }
+
+    @GetMapping("/update/flag")
+    fun updateStatusFlag(@RequestParam(value = "flagged", required = true) flagged: Boolean)
+    {
+        val temp = repository.findByMessageOrderByStatusID().first()
+    }
+
+    @GetMapping("/delete/all")
+    fun deleteAll(): String {
+        repository.deleteAll()
+        return "Deleted Everything!"
+    }
+
+    @GetMapping("/get/last")
+    fun getLast(): Long{
+        return getLastAdd()
+    }
+
+    @GetMapping("/count")
+    fun getCount(): Long {
+        return repository.count()
+    }
+
+    fun getLastAdd(): Long {
+        if (repository.count() != 0L) {
+            val temp = repository.findByMessageOrderByStatusID().first()
+            return temp.getStatusID()
+        } else {
+            return 0L
+        }
+    }
+}
---
+++ Backend/foodbuddy_mongo/src/main/kotlin/com/main/app/repository/StatusRepository.kt
@@ -0,0 +1,14 @@
+package com.main.fbBackend.repository
+
+import com.main.fbBackend.model.Status
+import kotlin.collections.MutableList
+import org.springframework.data.mongodb.repository.MongoRepository
+
+interface StatusRepository : MongoRepository<Status, String>{
+
+    fun findAllBy() : MutableList<Status>
+    fun findByName(name : String) : MutableList<Status>
+    fun findByMessage(message : String) : MutableList<Status>
+    fun findByFlagged(flagged : Boolean) : MutableList<Status>
+    fun findByMessageOrderByStatusID() : MutableList<Status>
+}
