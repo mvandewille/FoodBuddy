38a1409b3a2669a20fce69fa6c9fb4aee2e9e390
code-by-max <maw1@iastate.edu>
2020-02-17 11:05:37 -0600

    update

--- Experiments/MaxW/tests.py
+++ Experiments/MaxW/tests.py
@@ -1,6 +1,6 @@
 import requests
 import json
-URL = 'http://coms-309-hv-3.cs.iastate.edu:8080/user/find/all'
-r = requests.get(URL)
+URL = 'http://coms-309-hv-3.cs.iastate.edu:8080/user/auth'
+r = requests.get(URL, params={"email": "tdj1@iastate.edu", "password":"password"})
 print(r.status_code)
-print(r.text())
+print(r.json())
