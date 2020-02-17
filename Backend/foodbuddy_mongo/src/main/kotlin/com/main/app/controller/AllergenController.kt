985c41725456ea5ed1fdb0b42122520d599953b6
BuildTools <unconfigured@null.spigotmc.org>
2020-02-17 17:09:14 -0600

    add allergen functionality

--- Backend/foodbuddy_mongo/src/main/kotlin/com/main/app/controller/AllergenController.kt
+++ Backend/foodbuddy_mongo/src/main/kotlin/com/main/app/controller/AllergenController.kt
@@ -1,9 +1,11 @@
 package com.main.app.controller
 
 import com.main.app.JSON.AllergenJ
+import com.main.app.JSON.ResponseJ
 import com.main.app.model.Allergen
 import com.main.app.repository.AllergenRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -17,16 +19,40 @@ class AllergenController {
     @Autowired
     lateinit var repository: AllergenRepository
 
-    @PostMapping("/add")
-    fun addName(@RequestBody allergen: AllergenJ): String{
-        val t = Allergen(allergen.name)
-        repository.save(t)
+    @GetMapping("/find/all")
+    fun find(): String {
+        try {
+            var temp = repository.findAllBy()
 
-        return "Added new $t"
+            return stringify(temp)
+        }
+        catch (e: EmptyResultDataAccessException) {
+            return "No data!"
+        }
+    }
+
+    @PostMapping("/add")
+    fun addName(@RequestBody allergen: AllergenJ): ResponseJ {
+        try {
+            val temp = repository.findByName(allergen.name)
+            return ResponseJ(0, "Allergy already exists")
+        }
+        catch(e: EmptyResultDataAccessException) {
+            repository.save(Allergen(allergen.name))
+            return ResponseJ(1, "N/A")
+        }
     }
     @GetMapping("/delete/all")
     fun findAll() :String {
         repository.deleteAll()
         return "All allergies deleted!"
+    }
+
+    fun stringify(list: MutableList<Allergen>): String {
+        var r = ""
+        list.forEach{
+            r += it.toString() + "<br>"
+        }
+        return r
     }
 }
