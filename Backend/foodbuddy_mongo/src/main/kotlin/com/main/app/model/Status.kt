5b500d9a58ce6c88cc5d4fbf2883e85f27c5339b
code-by-max <maw1@iastate.edu>
2020-03-03 18:06:40 -0600

    test

--- Backend/foodbuddy_mongo/src/main/kotlin/com/main/app/model/Status.kt
+++ Backend/foodbuddy_mongo/src/main/kotlin/com/main/app/model/Status.kt
@@ -2,16 +2,21 @@ package com.main.app.model
 
 import org.springframework.data.annotation.Id
 
-class Status (@Id private val id: Long, private val email: String, private val name: String, private val message: String, private val flagged: Boolean) {
+class Status (@Id private val id: Long, private val email: String,  private val timestamp: String, private val message: String, private val flagged: Boolean) {
 
     constructor(id: Long, email: String, name: String, message: String)
         : this(id, email, name, message, false)
 
     override fun toString(): String {
-        return "Status[name=$name, message=$message, flagged=$flagged statusID=$id]"
+        return "Status[email=$email, message=$message, flagged=$flagged statusID=$id]"
     }
 
     fun getId() : Long{
         return this.id
+    }
+
+    //date format should be MM/dd/yyyy:hh-mm-ss
+    fun checkDate(date: String): Boolean {
+        return """^(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d${'$'}""".toRegex().matches(date)
     }
 }
