669f729a52f0d73c3f9409c32c18065549b833de
Sam Massey <samassey@iastate.edu>
2020-03-09 15:59:38 -0500

    Added all status class call tests

--- Backend/foodbuddy_mongo/src/test/kotlin/com/main/app/FoodbuddyApplicationTests.kt
+++ Backend/foodbuddy_mongo/src/test/kotlin/com/main/app/FoodbuddyApplicationTests.kt
@@ -2,6 +2,7 @@ package com.main.app
 
 import com.main.app.json.StatusJ
 import com.main.app.model.Status
+import com.main.app.model.User
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
@@ -43,5 +44,89 @@ class FoodbuddyApplicationTests {
         s.toJson()
         //verify if is called
         verify(s).toJson()
+    }
+
+    //TEST THE USER CLASS
+    @Test
+    fun TestUserGetFoods()
+    {
+        //Mock the object
+        val u = Mockito.mock(User::class.java)
+        //Call
+        u.getFoods()
+        //verify if is called
+        verify(u).getFoods()
+    }
+
+    @Test
+    fun TestUserGetFood()
+    {
+        //Mock the object
+        val u = Mockito.mock(User::class.java)
+        //Call
+        u.getFood("Apple")
+        //verify if is called
+        verify(u).getFood("Apple")
+    }
+
+    @Test
+    fun TestUserGetCalender()
+    {
+        //Mock the object
+        val u = Mockito.mock(User::class.java)
+        //Call
+        u.getCalendar()
+        //verify if is called
+        verify(u).getCalendar()
+    }
+
+    @Test
+    fun TestUserGetFollowing()
+    {
+        //Mock the object
+        val u = Mockito.mock(User::class.java)
+        //Call
+        u.getFollowing()
+        //verify if is called
+        verify(u).getFollowing()
+    }
+
+    @Test
+    fun TestUserToJson()
+    {
+        //Mock the object
+        val u = Mockito.mock(User::class.java)
+        //Call
+        u.toJson()
+        //verify if is called
+        verify(u).toJson()
+    }
+
+    @Test
+    fun TestUserToBasicJson()
+    {
+        //Mock the object
+        val u = Mockito.mock(User::class.java)
+        //Call
+        u.toBasicJson()
+        //verify if is called
+        verify(u).toBasicJson()
+    }
+
+    @Test
+    fun TestUserAddFollowing()
+    {
+        //Mock the object
+        val u = Mockito.mock(User::class.java)
+        //Call
+        u.addFollowing("Test")
+        //verify if is called
+        verify(u).addFollowing("Test")
+    }
+
+    @Test
+    fun TestUserSetExtras()
+    {
+        
     }
 }
