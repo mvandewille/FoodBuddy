cb428d6b42bbe3d3a87f38e5a9c459269bb0a19f
code-by-max <maw1@iastate.edu>
2020-04-14 10:22:25 -0600

    move websockets back to 444 :(

--- Backend/clientFiles/client.java
+++ Backend/clientFiles/client.java
@@ -27,6 +27,7 @@ public class client {
             while(in.hasNextLine() || !done) {
                 buffer = in.nextLine();
                 if(!buffer.equals("you are connected. Past messages are shown above")) {
+                    System.out.println(buffer);
                     System.out.println(parse(buffer));
                 }
                 else {
--- Backend/foodbuddy_mongo/src/main/kotlin/com/main/app/FoodbuddyApplication.kt
+++ Backend/foodbuddy_mongo/src/main/kotlin/com/main/app/FoodbuddyApplication.kt
@@ -2,8 +2,6 @@ package com.main.app
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import com.main.app.websocket.ClientHandler
-import com.main.app.websocket.Message
 import java.net.*
 import java.io.*
 import kotlin.system.exitProcess
@@ -13,38 +11,4 @@ class FoodbuddyApplication
 
 fun main(args: Array<String>) {
 	runApplication<FoodbuddyApplication>(*args)
-	println("WebSocket server starting...")
-	var serverSocket: ServerSocket? = null
-	var clients = mutableListOf<Socket>()
-	var messages = mutableListOf<String>()
-
-	//var fileName = "/Users/max/Documents/Random_Code/docker/websockets/msgLogs.txt"
-	var fileName = "/log.txt"
-	File(fileName).forEachLine {
-		messages.add(it)
-		Message(it.split(";")).print()
-	}
-	var clientNum = 0
-	try{
-		serverSocket = ServerSocket(8080)
-	} catch (e: IOException) {
-		println("Can't listen on port: 8080")
-		exitProcess(-1)
-	}
-
-	while(true) {
-		var clientSocket: Socket? = null
-
-		try {
-			clientSocket = serverSocket.accept()
-			clients.add(clientSocket)
-
-			var t = Thread(ClientHandler(clientSocket, clientNum, clients, messages))
-			t.start()
-			clientNum++
-		} catch (e: IOException) {
-			println("Accept failed: 8080")
-			exitProcess(-1)
-		}
-	}
 }
